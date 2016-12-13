fe7b07878838b30a4294ec5d0e9be015b636af56
sundalei <sundalei2011@163.com>
2016-12-13 23:26:08 +0800

    perfermance utils with react-addons-perf

--- add-on/src/index.js
+++ add-on/src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Perf from 'react-addons-perf';
 import App from './App';
+import FilterableProductTable from './PerformanceTools';
 import './index.css';
 
+const products = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
+
+Perf.start();
+
 ReactDOM.render(
-  <App />,
+  <FilterableProductTable products={products} />,
   document.getElementById('root')
 );
+
+Perf.stop();
+
+const measurements = Perf.getLastMeasurements();
+
+Perf.printInclusive(measurements);
+
+Perf.printExclusive(measurements);
+
+Perf.printWasted(measurements);
+
+Perf.printOperations(measurements);
+
+Perf.printDOM(measurements);
